79570a1a65445457f5ab051fcb1407bb8b002b6c
Jorge A Dominguez M <dominguezjorgea@gmail.com>
2023-12-20 15:36:14 -0500

    improved way to deal with Sets

--- src/datastructures/sets/MergeSort.java
+++ src/datastructures/sets/MergeSort.java
@@ -6,15 +6,12 @@ import java.util.stream.Collectors;
 public class MergeSort {
 
     public static void main(String[] args) {
-        ArrayList<Integer> list1 = new ArrayList<Integer>(Arrays.asList(7, -5, 3, 8, -4, 11, -19, 21));
-        ArrayList<Integer> list2 = new ArrayList<Integer>(Arrays.asList(6, 13, -7, 0, 11, -4, 3, -5));
+        List<Integer> list1 = new ArrayList<>(Arrays.asList(7, -5, 3, 8, -4, 11, -19, 21));
+        List<Integer> list2 = new ArrayList<>(Arrays.asList(6, 13, -7, 0, 11, -4, 3, -5));
 
         Set<Integer> mergeSet = new HashSet<>(list1);
 
-        for(int i=0; i<list2.size();i++ ){
-            Integer temp = list2.get(i);
-            mergeSet.add(temp);
-        }
+        mergeSet.addAll(list2);
 
         List<Integer> sortSet = mergeSet.stream().sorted().collect(Collectors.toList());
         System.out.println(sortSet);
